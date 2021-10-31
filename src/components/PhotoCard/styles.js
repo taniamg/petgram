55bb8ac4c57cee992ebb38c16cf8de6ddc341117
taniamg <taniamg81@gmail.com>
2021-10-31 02:21:28 +0200

    add animation photocard

--- src/components/PhotoCard/styles.js
+++ src/components/PhotoCard/styles.js
@@ -1,18 +1,5 @@
-import styled, { css, keyframes } from 'styled-components'
-
-const fadeInKeyframes = keyframes`
- from {
-    filter: blur(5px);
-    opacity: 0;
-  }
-
-  to {
-    filter: blur(0);
-    opacity: 1;
-  }
-`
-const fadeIn = ({ time = '1s', type = 'ease' } = {}) =>
-  css`animation: ${time} ${fadeInKeyframes} ${type};`
+import styled from 'styled-components'
+import { fadeIn } from '../../styles/animation'
 
 export const ImgWrapper = styled.div`
   border-radius: 10px;
